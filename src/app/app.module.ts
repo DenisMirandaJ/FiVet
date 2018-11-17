14857087a4726a7640da55625a5c1551a4c0b693
Felipe Varas Jara <fexnitro@me.com>
2018-11-17 15:09:14 -0300

    Merged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,18 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PacienteFormComponent } from './paciente-form/paciente-form.component';
-<<<<<<< HEAD
 import { RegistroMedicoFormsComponent } from './registro-medico-forms/registro-medico-forms.component';
-=======
 import { ClienteSmallFormComponent } from './cliente-small-form/cliente-small-form.component';
 import { PacienteSearchBoxComponent } from './paciente-search-box/paciente-search-box.component';
 import { PacienteSearchBoxService } from './paciente-search-box/paciente-search-box.service';
->>>>>>> 5257ebb735f63f9aa57c22affaab27223914bae4
 
 @NgModule({
   declarations: [
     AppComponent,
     PacienteFormComponent,
-<<<<<<< HEAD
-    RegistroMedicoFormsComponent
-=======
+    RegistroMedicoFormsComponent,
     ClienteSmallFormComponent,
     PacienteSearchBoxComponent
->>>>>>> 5257ebb735f63f9aa57c22affaab27223914bae4
   ],
   imports: [
     BrowserModule,
